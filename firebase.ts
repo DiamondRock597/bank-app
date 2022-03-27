fc919e33477b62ae6a441900655b6d8f393e1af8
DiamondRock597 <diamondrock17rocker@gmail.com>
2022-03-27 18:51:31 +0300

    first init

---
+++ firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDYS-Wg6yaRFTuAxhJmukFhuYom6UQbrNA",
+    authDomain: "tinkoff-8d153.firebaseapp.com",
+    projectId: "tinkoff-8d153",
+    storageBucket: "tinkoff-8d153.appspot.com",
+    messagingSenderId: "46821099816",
+    appId: "1:46821099816:web:1b42cce06c97f4ff9f281d"
+};
+
+export const app = initializeApp(firebaseConfig);
